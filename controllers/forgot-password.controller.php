19501474c3606e3a0f53cf0aad35450c7a7d4e46
grntbg <13023526+grntbg@users.noreply.github.com>
2022-01-12 20:26:25 -0600

    Extend flood control to the forgot password controller

--- controllers/forgot-password.controller.php
+++ controllers/forgot-password.controller.php
@@ -53,6 +53,43 @@ function init()
 	
 	// If they've submitted their email to get a password reset link, email one to them!
 	if (isset($_POST["email"])) {
+
+		// If we're counting logins per minute, impose some flood control measures.
+		if (!isset($cookie) and $config["loginsPerMinute"] > 0) {
+
+			// If we have a record of their logins in the session, check how many logins they've performed in the last
+			// minute.
+			if (!empty($_SESSION["logins"])) {
+				// Clean anything older than 60 seconds out of the logins array.
+				foreach ($_SESSION["logins"] as $k => $v) {
+					if ($v < time() - 60) unset($_SESSION["logins"][$k]);
+				}
+				// Have they performed >= $config["loginsPerMinute"] logins in the last minute? If so, don't continue.
+				if (count($_SESSION["logins"]) >= $config["loginsPerMinute"]) {
+					$this->eso->message("waitToLogin", true, array(60 - time() + min($_SESSION["logins"])));
+					return;
+				}
+			}
+
+			// However, if we don't have a record in the session, use the MySQL logins table.
+			else {
+				// Get the user's IP address.
+				$ip = (int)ip2long($_SESSION["ip"]);
+				// Have they performed >= $config["loginsPerMinute"] logins in the last minute?
+				if ($this->eso->db->result("SELECT COUNT(*) FROM {$config["tablePrefix"]}logins WHERE ip=$ip AND loginTime>UNIX_TIMESTAMP()-60", 0) >= $config["loginsPerMinute"]) {
+					$this->eso->message("waitToLogin", true, 60);
+					return;
+				}
+				// Log this attempt in the logins table.
+				$this->eso->db->query("INSERT INTO {$config["tablePrefix"]}logins (ip, loginTime) VALUES ($ip, UNIX_TIMESTAMP())");
+				// Proactively clean the logins table of logins older than 60 seconds.
+				$this->eso->db->query("DELETE FROM {$config["tablePrefix"]}logins WHERE loginTime<UNIX_TIMESTAMP()-60");
+			}
+
+			// Log this attempt in the session array.
+			if (!isset($_SESSION["logins"]) or !is_array($_SESSION["logins"])) $_SESSION["logins"] = array();
+			$_SESSION["logins"][] = time();
+		}
 		
 		// Find the member with this email.
 		$result = $this->eso->db->query("SELECT memberId, name, email FROM {$config["tablePrefix"]}members WHERE email='{$_POST["email"]}'");
