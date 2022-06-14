85620f5d672030d536eb6876d2eced9de430fab8
andromidaZ <101992850+andromidaZ@users.noreply.github.com>
2022-06-13 18:07:29 -0700

    Add an icon for deleting a search

--- views/searchForm.inc.php
+++ views/searchForm.inc.php
@@ -28,7 +28,7 @@ if(!defined("IN_ESO"))exit;
 <div>
 <input id='searchText' name='search' type='text' class='text' value='<?php echo @$_SESSION["search"];?>' spellcheck='false'/>
 <div class='fr'>
-<a id='reset' href='<?php echo makeLink("search","");?>'>x</a>
+<a id='reset' href='<?php echo makeLink("search","");?>'><i class="icon-trash"></i></a>
 <?php echo $this->eso->skin->button(array("id"=>"submit","name"=>"submit","value"=>$language["Search"],"class"=>"big"));?>
 <?php if($this->eso->action=="search"):?>
 <?php echo $this->eso->skin->button(array("id"=>"new","name"=>"new","value"=>$language["Start a conversation"],"class"=>"big"));?>
